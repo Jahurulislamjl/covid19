8adfb5e7384952e117ef0e5eb14cb8c3fa3a4fd7
Jahurul Islam <43181519+jahurulislamjl@users.noreply.github.com>
2020-03-20 21:32:20 +0600

    upload

---
+++ src/components/myHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <header class="header-nav p-2">
+
+            <div class="header-brand">
+
+                <div class="brand-logo jp-3">
+                    <img src="../assets/images/corona.png" class="hd-logo1 d-none" width="auto" height="30" alt="">
+                    <img src="../assets/images/COVID-19.png" class="hd-logo2" width="auto" height="33" alt="">
+                </div>
+
+                <div class="jp-3">
+                    <i id="sidebarCollapse"  class="mdi mdi-view-headline"></i>
+                </div>
+            </div>
+
+            <div class="header-content">
+                <h3>Icon</h3>
+            </div>
+
+        </header>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "myHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/dashboard.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+
+        <section id="dashboard">
+            <div class="container">
+
+                <p class="heading pt-5">Dashboard For Bangladesh</p>
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card card-1">
+                            <div class="card-body">
+                                <p class="title">total death</p>
+                                <p class="total-count">500</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-2">
+                            <div class="card-body">
+                                <p class="title">total death</p>
+                                <p class="total-count">500</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-3">
+                            <div class="card-body">
+                                <p class="title">total death</p>
+                                <p class="total-count">500</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row py-5">
+                    <div class="latest-update col">
+                        <div class="title">
+                            today's update
+                        </div>
+                        <div class="latest-news">
+                            Today, one person death by corona virus.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "dashboard"
+    }
+</script>
+
+<style scoped>
+
+</style>
